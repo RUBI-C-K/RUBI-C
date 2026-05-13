d1403e0b5723ed33fd8f5558f5fc2fe622e47d4d
RUBI-C-K <RUBIC-CONT@proton.me>
2026-05-12 22:56:58 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include "rubic_core.h"
+
+int main(int argc, char* argv[]) {
+    try {
+        // Verificar si  tenemos sudo
+        if (geteuid() != 0) {
+            std::cout << YELLOW << "\n⚠ Se requieren permisos de administrador" << RESET << std::endl;
+            std::cout << CYAN << "Reiniciando con sudo..." << RESET << std::endl;
+            
+            // Reconstruir con sudo
+            std::string cmd = "sudo ";
+            cmd += argv[0];
+            
+            
+            for (int i = 1; i < argc; i++) {
+                cmd += " '" + std::string(argv[i]) + "'";
+            }
+            
+            int ret = system(cmd.c_str());
+            return ret;
+        }
+        
+        // Ya hay sudo, continuar normalmente
+        std::cout << "\033[2J\033[1;1H";
+        Memguardin::lockProcess();
+        CrashHnd::setup();
+
+        UserMgr um;
+        um.run();
+    } 
+    catch (const std::exception& e) {
+        std::cerr << RED << "\n✗ ERROR: " << e.what() << RESET << std::endl;
+        Memguardin::unlockProcess();
+        return 1;
+    }
+    catch (...) {
+        std::cerr << RED << "\n✗ ERROR DESCONOCIDO" << RESET << std::endl;
+        Memguardin::unlockProcess();
+        return 1;
+    }
+
+    Memguardin::unlockProcess();
+    return 0;
+}
